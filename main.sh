960e4d48e5d39f4840cf327ecab636de10bc03b1
Evan Tremblay <evan.tremblay@stu.ocsb.ca>
2025-08-06 21:34:41 -0400

    Update file: /home/mrcodester/code/main.sh

--- main.sh
+++ main.sh
@@ -3,18 +3,18 @@
 CENTER_X=500
 CENTER_Y=1000
 RADIUS=200
-STEPS=36  # More steps = smoother circle
-DELAY=15  # ms between segments
+STEPS=60      # More steps = smoother circle
+DURATION=10   # ms for each swipe
 
 for ((i=0; i<$STEPS; i++)); do
-    angle1=$(echo "scale=6; 2*3.1415926*$i/$STEPS" | bc -l)
-    angle2=$(echo "scale=6; 2*3.1415926*($i+1)/$STEPS" | bc -l)
+    theta1=$(echo "2 * 3.14159 * $i / $STEPS" | bc -l)
+    theta2=$(echo "2 * 3.14159 * ($i + 1) / $STEPS" | bc -l)
 
-    x1=$(echo "$CENTER_X + $RADIUS * c($angle1)" | bc -l | awk '{printf "%d", $0}')
-    y1=$(echo "$CENTER_Y + $RADIUS * s($angle1)" | bc -l | awk '{printf "%d", $0}')
-    x2=$(echo "$CENTER_X + $RADIUS * c($angle2)" | bc -l | awk '{printf "%d", $0}')
-    y2=$(echo "$CENTER_Y + $RADIUS * s($angle2)" | bc -l | awk '{printf "%d", $0}')
+    x1=$(echo "$CENTER_X + $RADIUS * c($theta1)" | bc -l | awk '{printf "%d", $0}')
+    y1=$(echo "$CENTER_Y + $RADIUS * s($theta1)" | bc -l | awk '{printf "%d", $0}')
+    x2=$(echo "$CENTER_X + $RADIUS * c($theta2)" | bc -l | awk '{printf "%d", $0}')
+    y2=$(echo "$CENTER_Y + $RADIUS * s($theta2)" | bc -l | awk '{printf "%d", $0}')
 
-    adb shell input swipe $x1 $y1 $x2 $y2 $DELAY
+    adb shell input swipe $x1 $y1 $x2 $y2 $DURATION
 done
 
